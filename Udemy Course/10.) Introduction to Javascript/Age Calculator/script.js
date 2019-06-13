325aaa3faacaec316be4a25a79c3a3f67e608a93
AustinHodgin <hodgina@oregonstate.edu>
2019-06-13 16:01:17 -0700

    Added finished introduction to javascript section

---
+++ Udemy Course/10.) Introduction to Javascript/Age Calculator/script.js
@@ -0,0 +1,4 @@
+var age = prompt("What is your age in years?");
+daysLived = age * 356.25;
+alert("You have lived roughly " + daysLived + " days")
+console.log("You have lived roughly " + daysLived + " days")
---
+++ Udemy Course/10.) Introduction to Javascript/JS Stalker Exercise/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Hello, " + firstName + " " + lastName);
+console.log("It's amazing you're " + age + " years old!");
