dddf97b420e04877aa2e643472e94b91a5e3ff6a
AustinHodgin <hodgina@oregonstate.edu>
2019-08-02 17:15:35 -0700

    Renamed campgrounds.ejs to index.ejs. Added ability to add campgrounds to mongo database

--- Udemy Course/30.) Yelp Camp Data Persistence/My Version/index.js
+++ Udemy Course/30.) Yelp Camp Data Persistence/My Version/index.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
-var campgrounds = [
-    {name:"Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/26/23/32/camp-1163419_1280.jpg"},
-    {name:"Granite Hill", image: "https://cdn.pixabay.com/photo/2016/12/08/17/45/lake-sara-1892494_960_720.jpg"},
-    {name:"Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2017/10/07/01/01/bach-leek-2825197_960_720.jpg"},
-    {name:"Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/26/23/32/camp-1163419_1280.jpg"},
-    {name:"Granite Hill", image: "https://cdn.pixabay.com/photo/2016/12/08/17/45/lake-sara-1892494_960_720.jpg"},
-    {name:"Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2017/10/07/01/01/bach-leek-2825197_960_720.jpg"},
-    {name:"Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/26/23/32/camp-1163419_1280.jpg"},
-    {name:"Granite Hill", image: "https://cdn.pixabay.com/photo/2016/12/08/17/45/lake-sara-1892494_960_720.jpg"},
-    {name:"Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2017/10/07/01/01/bach-leek-2825197_960_720.jpg"},
-    {name:"Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/26/23/32/camp-1163419_1280.jpg"},
-    {name:"Granite Hill", image: "https://cdn.pixabay.com/photo/2016/12/08/17/45/lake-sara-1892494_960_720.jpg"},
-    {name:"Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2017/10/07/01/01/bach-leek-2825197_960_720.jpg"},
-    {name:"Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/26/23/32/camp-1163419_1280.jpg"},
-    {name:"Granite Hill", image: "https://cdn.pixabay.com/photo/2016/12/08/17/45/lake-sara-1892494_960_720.jpg"},
-    {name:"Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2017/10/07/01/01/bach-leek-2825197_960_720.jpg"},
+mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true});
 
-];
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Granite Hill",
+//         image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732b7cd09f4bc351_340.jpg",
+//         description: "This is a huge granite hill, no bathrooms. No water, Beautiful granite!"
+//     }
+//     , function(err, campground){
+//     if(err){
+//         console.log(err)
+//     } else{
+//         console.log("New Created Campground");
+//         console.log(campground);
+//         }
+//     }
+// );
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -29,21 +38,57 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+//Index
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    //get all campgrounds from db
+    Campground.find({}, function(err, campgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("index", {campgrounds: campgrounds});
+        }
+    });
 });
 
+//Create - add new campground to the DB
 app.post("/campgrounds", function(req, res){
     //get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description : desc}
+   
+    //add to db
+    Campground.create(newCampground, function(err, campground){
+        if(err){
+            console.log(err)
+        } else{
+            console.log("New Created Campground");
+            console.log(campground);
+            }
+        }
+    );
     //redirect to campgrounds
     res.redirect("campgrounds")
 });
+
+//New - shows form to add new campground
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
+});
+
+//Show - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    //find  the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            //render the page with info from ID
+            res.render("show", {campground: foundCampground});
+        }
+    });
+    
 });
 
 app.listen(port, () => console.log(`Yelp Camp Page Started on port ${port}!`) );
