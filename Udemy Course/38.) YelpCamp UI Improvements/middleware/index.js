7fa9bc0fb90ba9c0a2fbcb8fdbc78e8f41e225f3
AustinHodgin <hodgina@oregonstate.edu>
2019-08-13 20:15:05 -0700

    Added fix to bug where changing the url for campground could crash the server

--- Udemy Course/38.) YelpCamp UI Improvements/middleware/index.js
+++ Udemy Course/38.) YelpCamp UI Improvements/middleware/index.js
@@ -10,6 +10,12 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next){
                 req.flash("error", "Campground not found");
                 res.redirect("back");
             }else{
+                // Added this block, to check if foundCampground exists, and if it doesn't to throw an error via connect-flash and send us back to the homepage
+                if (!foundCampground) {
+                    req.flash("error", "Item not found.");
+                    return res.redirect("back");
+                }
+
                 // does ueser own campground
                 if(foundCampground.author.id.equals(req.user._id)){
                     next();
@@ -31,15 +37,24 @@ middlewareObj.checkCommentOwnership = function (req, res, next){
             if(err){
                 res.redirect("back");
             }else{
+
+                // Added this block, to check if foundCampground exists, and if it doesn't to throw an error via connect-flash and send us back to the homepage
+                if (!foundCampground) {
+                    req.flash("error", "Item not found.");
+                    return res.redirect("back");
+                }
+
                 // does ueser own comment
                 if(foundComment.author.id.equals(req.user._id)){
                     next();
                 }else{
+                    req.flash("error", "Permmison Denied");
                     res.redirect("back");
                 }
             }
         });
     }else{
+        req.flash("error", "You need to be logged in to do that");
         res.redirect("back");
     }
 }
